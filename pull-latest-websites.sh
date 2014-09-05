240a0991f5eb7ded0e8237f6d82a7f65d1a44184
Keidrych Anton-Oates <alaztiest@gmail.com>
2014-09-05 20:15:30 +0000

    Custom Websites are enabled, and able to be synced

--- pull-latest-websites.sh
+++ pull-latest-websites.sh
@@ -2,3 +2,8 @@ for d in `find . -type f -name 'g2g.wordpress' |sed 's#\(.*\)/.*#\1#' |sort -u`
 do
     ( echo $d && cd $d && git pull )
 done
+for d in `find . -type f -name 'sync-remote.sh' |sed 's#\(.*\)/.*#\1#' |sort -u`
+do
+    ( echo $d && cd $d && git fetch upstream && git merge upstream/master )
+	echo "merging upstream changes into custom distribution"
+done
