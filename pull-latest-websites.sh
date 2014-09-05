70816ae39d4dcd45ec803b3f6ed47ec5e6363a2a
Keidrych Anton-Oates <alaztiest@gmail.com>
2014-09-05 21:46:43 +0000

    Custom Websites Enabled: Luvurday, inspiredword, inspiredword-curate

--- pull-latest-websites.sh
+++ pull-latest-websites.sh
@@ -4,6 +4,6 @@ do
 done
 for d in `find . -type f -name 'sync-remote.sh' |sed 's#\(.*\)/.*#\1#' |sort -u`
 do
-    ( echo $d && cd $d && git fetch upstream && git merge upstream/master )
+    ( echo $d && cd $d && ./sync-remote.sh && git fetch upstream && git merge upstream/master )
 	echo "merging upstream changes into custom distribution"
 done
