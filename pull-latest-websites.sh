b6737964a0e538440c2f6f1460764da11b8959c1
Keidrych Anton-Oates <alaztiest@gmail.com>
2014-09-06 01:10:06 +0000

    Remove migrate from servers

--- pull-latest-websites.sh
+++ pull-latest-websites.sh
@@ -1,9 +1,49 @@
-for d in `find . -type f -name 'g2g.wordpress' |sed 's#\(.*\)/.*#\1#' |sort -u`
-do
-    ( echo $d && cd $d && git pull )
-done
 for d in `find . -type f -name 'sync-remote.sh' |sed 's#\(.*\)/.*#\1#' |sort -u`
 do
-    ( echo $d && cd $d && ./sync-remote.sh && git fetch upstream && git merge upstream/master )
-	echo "merging upstream changes into custom distribution"
+	echo $d
+	cd $d
+	if [ ! -f ./synched.wordpress ]; then
+		git clean -df
+		./sync-remote.sh
+		touch g2g.wordpress
+	else
+		git checkout master
+		git reset --hard
+		git branch -D synched
+		rm -rf synched.wordpress
+		git pull
+	fi
+		git branch synched
+		git checkout synched
+		git fetch --depth 1 wordpress $1-branch:synchedwordpress
+		git fetch --depth 1 upstream master:synchedupstream
+		git merge -q -X theirs -m "synchedupstream" synchedupstream
+		git merge -q -X theirs -m "synchedwordpress" synchedwordpress
+		touch synched.wordpress
+		#remote wordpress sync to differentiate the custom from original
+		rm -rf sync-wordpress.sh
+		cd ..
+done
+
+for d in `find . -type f -name 'sync-wordpress.sh' |sed 's#\(.*\)/.*#\1#' |sort -u`
+do
+	echo $d
+	cd $d
+	if [ ! -f $d/synched.wordpress ]; then
+		git clean -df
+		./sync-wordpress.sh
+		touch g2g.wordpress
+	else
+		git checkout master
+		git reset --hard
+		git branch -D synched
+		rm -rf synched.wordpress
+		git pull
+	fi
+		git branch synched
+		git checkout synched
+		git fetch --depth 1 wordpress $1-branch:synchedwordpress
+		git merge -q -X theirs -m "synchedupstream" synchedwordpress
+		touch synched.wordpress
+		cd ..
 done
