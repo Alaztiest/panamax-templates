93229964d3144a761d1a5f0d8daa976488247e46
Keidrych Anton-Oates <alaztiest@gmail.com>
2014-09-23 16:21:20 +0000

    Websites with custom repo and error recognition

--- add-website.sh
+++ add-website.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
+if [ $# -eq 0 ]; then
+    echo "No arguments provided"
+    echo "Argument 1: directory to clone into"
+    echo "Argument 2: tayloredtechnology/$2.git custom git repository"
+    exit 1
+fi
+
+if [ $# -eq 1 ]; then
+    echo "Argument 2: tayloredtechnology/$2.git custom git repository has not been specified"
+    echo "No Custom Repistory Specified"
+    echo ""
+fi
+
 if [ ! -d "gitcaches/startupbooster.reference" ]; then
   git clone --mirror https://TT-API:2ppROmtPfA5mKKFUagA7XGut@bitbucket.org/NURVING/startup-booster.git gitcaches/startupbooster.reference
 fi
 
-git clone git clone --reference gitcaches/startupbooster.reference https://TT-API:2ppROmtPfA5mKKFUagA7XGut@bitbucket.org/NURVING/startup-booster.git $1
+git clone --reference gitcaches/startupbooster.reference https://TT-API:2ppROmtPfA5mKKFUagA7XGut@bitbucket.org/NURVING/startup-booster.git $1
 cd $1
-git remote add sync-custom https://TT-API:2ppROmtPfA5mKKFUagA7XGut@bitbucket.org/tayloredtechnology/$1.git
+git remote add sync-custom https://TT-API:2ppROmtPfA5mKKFUagA7XGut@bitbucket.org/tayloredtechnology/$2.git
